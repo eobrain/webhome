c45f49c20a6b13db23fe671f7041e741e0ef7589
Eamonn O'Brien-Strain <eobrain@gmail.com>
2019-09-30 22:38:23 -0700

    Add article on swiping.

--- js/swipe.js
+++ js/swipe.js
@@ -1,8 +1,13 @@
+
+/* global Hammer */
+
 const mc = new Hammer(document.body)
 
 mc.on('swipe', (ev) => {
   const dir = (ev.deltaX < 0) ? 'next' : 'prev'
-  const href = document.getElementById(dir).href
   document.body.classList.add('move-' + dir)
-  window.location = href
+  const a = document.getElementById(dir)
+  if (a) {
+    window.location = a.href
+  }
 })
