68ee4bf648fba76f30b3ba9abb953b2b786b8251
Eamonn O'Brien-Strain <eobrain@gmail.com>
2019-10-01 22:10:12 -0700

    Tweak animations

--- js/swipe.js
+++ js/swipe.js
@@ -5,9 +5,9 @@ const mc = new Hammer(document.body)
 
 mc.on('swipe', (ev) => {
   const dir = (ev.deltaX < 0) ? 'next' : 'prev'
-  document.body.classList.add('move-' + dir)
   const a = document.getElementById(dir)
   if (a) {
+    document.body.classList.add('animate-' + dir)
     window.location = a.href
   }
 })
