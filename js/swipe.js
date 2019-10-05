c44c27c9ed8b03b6d5de7e464df2e64e92bcc808
Eamonn O'Brien-Strain <eobrain@gmail.com>
2019-10-05 13:05:21 -0700

    Add on-load animation on swipe. Add IP echo page.

--- js/swipe.js
+++ js/swipe.js
@@ -11,3 +11,25 @@ mc.on('swipe', (ev) => {
     window.location = a.href
   }
 })
+
+document.body.ontouchstart = (event) => {
+  if (event.targetTouches.length === 1) {
+    const touch0 = event.targetTouches[0]
+    const x0 = touch0.pageX
+    let x = x0
+
+    document.body.ontouchmove = (event) => {
+      if (event.targetTouches.length === 1) {
+        const touch = event.targetTouches[0]
+        x = touch.pageX
+        const dx = Math.max(-20, Math.min(20, x - x0))
+        document.body.style.transform = `translateX(${dx}px)`
+      }
+    }
+    document.body.ontouchend = (event) => {
+      document.body.style.transform = ''
+      document.body.ontouchmove = null
+      document.body.ontouchend = null
+    }
+  }
+}
