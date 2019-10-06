5e2f800876d50619a45c7b098ddcca330061c0e1
Eamonn O'Brien-Strain <eobrain@gmail.com>
2019-10-05 17:28:14 -0700

    Add animation for going into child page.

---
+++ js/blocking.js
@@ -0,0 +1,14 @@
+const slideIn = (cssClass) => {
+  document.body.classList.add(cssClass)
+  document.querySelector('.' + cssClass).addEventListener('animationend', () => {
+    document.body.classList.remove(cssClass)
+    document.body.onfinish = null
+  })
+}
+
+if (window.location.hash) {
+  const dir = window.location.hash.slice(1)
+  slideIn(`animate-load-${dir}`)
+} else {
+  slideIn('animate-load-child')
+}
