a1e661701f1c751a08513e4efa796aee56c82eaa
Eamonn O'Brien-Strain <eobrain@gmail.com>
2019-10-06 16:43:29 -0700

    Defer images below the fold.

---
+++ _includes/home.js
@@ -0,0 +1,11 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(function () { console.log('Service Worker Registered') })
+}
+
+document.body.onload = () => {
+  document.querySelectorAll('img[data-defer]').forEach((img) => {
+    img.src = img.dataset.defer
+  })
+}
