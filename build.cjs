6f04f7608045fd3e00d80cc35de603df8bf8192a
Eamonn O'Brien-Strain <eobrain@gmail.com>
2020-03-07 23:20:34 -0800

    Update build file to new passive format.

---
+++ build.cjs
@@ -0,0 +1,33 @@
+const COMPILEJS = 'java -jar tools/closure/closure-compiler-v20190929.jar -O ADVANCED'
+const COMPILED = ['sw.js', '_includes/home.js', '_includes/post.js', '_includes/post_blocking.js']
+const compile = `${COMPILEJS} --js $< --js_output_file $@`
+
+module.exports = {
+  serve: {
+    deps: ['compile'],
+    exec: 'jekyll serve --port 8000 --strict_front_matter --watch --drafts'
+  },
+
+  compile: { deps: COMPILED },
+
+  'sw.js': { deps: ['js/sw.js'], exec: compile },
+
+  '_includes/%.js': { deps: ['js/%.js'], exec: compile },
+
+  prodbuild: {
+    deps: ['compile'],
+    exec: `
+      JEKYLL_ENV=production jekyll build --strict_front_matter --config _config.yml,_secret.yml
+      minify -r -o _site/ _site
+    `
+  },
+
+  deploy: { deps: ['prodbuild'], exec: 'firebase deploy' },
+
+  clean: {
+    exec: `
+      jekyll clean
+      rm -f ${COMPILED.join(' ')}
+    `
+  }
+}
