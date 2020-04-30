d10a9f02a3ec34b9339ba9a84e4302be98ff373a
Eamonn O'Brien-Strain <eobrain@gmail.com>
2020-04-30 09:04:51 -0700

    Fix graph responsiveness.

--- extra/covidgrowth/index.js
+++ extra/covidgrowth/index.js
@@ -1,42 +1,53 @@
 /* global DATA Chart
    articleElement */
 
-(async () => {
-  // Credit https://stackoverflow.com/a/46099731/978525
-  const DAYS_BEFORE_EPOCH = 70 * 365 + 19
-  const HOUR = 60 * 60 * 1000
-  const excelDate2js = excelDate =>
-    new Date(Math.round((excelDate - DAYS_BEFORE_EPOCH) * 24 * HOUR) + 12 * HOUR)
-  const excelDates = DATA.rows[0]
-  const serieses = DATA.rows.slice(1)
-  const colors = DATA.colors
+// Credit https://stackoverflow.com/a/46099731/978525
+const DAYS_BEFORE_EPOCH = 70 * 365 + 19
+const HOUR = 60 * 60 * 1000
+const excelDate2js = excelDate =>
+  new Date(Math.round((excelDate - DAYS_BEFORE_EPOCH) * 24 * HOUR) + 12 * HOUR)
+const excelDates = DATA.rows[0]
+const serieses = DATA.rows.slice(1)
+const colors = DATA.colors
 
-  const labels = excelDates.map(x => excelDate2js(x).toLocaleDateString())
+const dates = excelDates.map(x => excelDate2js(x))
+const labels = DATA.columns.slice(1)
 
-  const drawGraph = datasets => {
-    const canvasElement = document.createElement('CANVAS')
-    articleElement.appendChild(canvasElement)
-    const ctx = canvasElement.getContext('2d')
+const drawGraph = datasets => {
+  const canvasElement = document.createElement('CANVAS')
+  articleElement.appendChild(canvasElement)
+  const ctx = canvasElement.getContext('2d')
 
-    return new Chart(ctx, {
-      type: 'line',
-      data: { labels, datasets },
-      options: {}
-    })
-  }
+  return new Chart(ctx, {
+    type: 'line',
+    data: { datasets },
+    options: {
+      aspectRatio: 1,
+      scales: {
+        xAxes: [{
+          type: 'time',
+          time: {
+            unit: 'week'
+          }
+        }]
+      }
+    }
+  })
+}
 
-  drawGraph(serieses.map((row, i) => ({
-    label: DATA.columns[i + 1],
+const toPoints = (y, i) => ({ t: dates[i], y })
+
+drawGraph(serieses.map((row, i) => ({
+  label: labels[i],
+  backgroundColor: colors[i] + '40',
+  borderColor: colors[i],
+  data: row.map(toPoints)
+})))
+serieses.forEach((series, i) => {
+  drawGraph([{
+    label: labels[i],
     backgroundColor: colors[i] + '40',
     borderColor: colors[i],
-    data: row
-  })))
-  serieses.forEach((series, i) => {
-    drawGraph([{
-      label: DATA.columns[i + 1],
-      backgroundColor: colors[i] + '40',
-      borderColor: colors[i],
-      data: series
-    }])
-  })
-})()
+    data: series.map(toPoints)
+  }])
+})
