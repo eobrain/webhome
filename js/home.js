900e2833589735560481f9cc56375b0f575b4dd6
Eamonn O'Brien-Strain <eobrain@gmail.com>
2020-06-20 19:21:40 -0700

    More aggressive image deferral.

--- js/home.js
+++ js/home.js
@@ -10,7 +10,27 @@ const application = Stimulus.Application.start()
 
 application.register('image', class extends Stimulus.Controller {
   defer () {
-    this.element.src = this.data.get('src')
-    this.element.removeAttribute('data-controller') // disable this controller
+    if (isElementInViewport(this.element)) {
+      this.element.src = this.data.get('src')
+      this.element.removeAttribute('data-controller') // disable this controller
+    }
+  }
+
+  connect () {
+    this.defer()
   }
 })
+
+// Credit: Dan https://stackoverflow.com/a/7557433/978525
+const isElementInViewport = el => {
+  const rect = el.getBoundingClientRect()
+  const { innerHeight, innerWidth } = window
+  const { clientHeight, clientWidth } = document.documentElement
+
+  return (
+    rect.bottom >= 0 &&
+      rect.left >= 0 &&
+      rect.top <= (innerHeight || clientHeight) &&
+      rect.right <= (innerWidth || clientWidth)
+  )
+}
