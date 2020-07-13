8afc40ae9c54f9189b682642adbce749400a6e24
Eamonn O'Brien-Strain <eobrain@gmail.com>
2020-07-12 21:58:08 -0700

    Track shares.

--- js/every.js
+++ js/every.js
@@ -1,3 +1,5 @@
+/* global gtag */
+
 const stripTrailingSlash = url => url.replace(/\/$/, '')
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -7,5 +9,18 @@ document.addEventListener('DOMContentLoaded', () => {
       aElement.removeAttribute('href')
       aElement.classList.add('current')
     }
+  })
+  document.querySelectorAll('a.tweet').forEach(aElement => {
+    aElement.addEventListener('click', () => {
+      const params = new URL(aElement.href).searchParams
+      const path = new URL(params.get('url')).pathname
+      console.log('path=', path)
+      console.log('text=', params.get('text'))
+      gtag('event', 'share', {
+        method: 'Twitter',
+        content_id: path,
+        description: params.get('text')
+      })
+    }, false)
   })
 })
