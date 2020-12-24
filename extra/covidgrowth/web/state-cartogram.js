40908d491e75c9926a3cf707a652857641eb154f
Eamonn O'Brien-Strain <eob@well.com>
2020-12-24 10:55:28 -0800

    Latest data.

--- extra/covidgrowth/web/state-cartogram.js
+++ extra/covidgrowth/web/state-cartogram.js
@@ -4,7 +4,7 @@ import links from './state-cartogram-links.js'
 import latlons from './state-cartogram-latlons.js'
 import nodesWithoutLatlon from './state-cartogram-nodes.js'
 
-const lastYear = 336
+const lastYear = 337
 
 /* global d3, timeElement, figureElement, runningElement */
 
